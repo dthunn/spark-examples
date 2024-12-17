45a409000d85fb759aadb72c28b4fcb6fd3675e6
Dylan Thunn <49996239+dthunn@users.noreply.github.com>
2024-12-17 07:59:13 -0700

    hello spark unit tests

--- hello_spark/app.py
+++ hello_spark/app.py
@@ -3,28 +3,30 @@ from pyspark.sql import *
 from lib.logger import Log4j
 from lib.utils import *
 
-if __name__ == "__main__":
-   conf = get_spark_app_config()
 
-   spark = SparkSession.builder \
+if __name__ == "__main__":
+    conf = get_spark_app_config()
+
+    spark = SparkSession.builder \
         .master("local[3]") \
         .config(conf=conf) \
         .appName("Hello Spark") \
         .getOrCreate()   
 
-   logger = Log4j(spark)
+    logger = Log4j(spark)
 
-   if len(sys.argv) != 2:
+    if len(sys.argv) != 2:
         logger.error("Usage: HelloSpark <filename>")
         sys.exit(-1)
 
-   logger.info("Starting HelloSpark")
+    logger.info("Starting HelloSpark")
+ 
+    survey_raw_df = load_survey_df(spark, sys.argv[1])
+    # Not normally needed but for testing purposes  
+    partitioned_survey_df = survey_raw_df.repartition(2)
+    count_df = count_by_country(partitioned_survey_df)
+    count_df.show()
 
-   survey_raw_df = load_survey_df(spark, sys.argv[1])
-   partitioned_survey_df = survey_raw_df.repartition(2)
-   count_df = count_by_country(partitioned_survey_df)
-   count_df.show()
-
-   logger.info("Finished HelloSpark")
+    logger.info("Finished HelloSpark")
    
-   spark.stop()
+    spark.stop()

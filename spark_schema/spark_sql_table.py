b30e61938ea4da3fd5f8ab58691dc057349f9f88
Dylan Thunn <49996239+dthunn@users.noreply.github.com>
2024-12-19 10:56:54 -0700

    data sink demo

---
+++ spark_schema/spark_sql_table.py
@@ -0,0 +1,27 @@
+# from pyspark.sql import *
+
+# from lib.logger import Log4j
+
+# if __name__ == "__main__":
+#     spark = SparkSession \
+#         .builder \
+#         .master("local[3]") \
+#         .appName("SparkSQLTableDemo") \
+#         .config("spark.driver.extraJavaOptions", "-Dlog4j.configuration=file:log4j.properties -Dspark.yarn.app.container.log.dir=app-logs -Dlogfile.name=hello-rdd") \
+#         .enableHiveSupport() \
+#         .getOrCreate()
+
+#     logger = Log4j(spark)
+
+#     flightTimeParquetDF = spark.read \
+#         .format("parquet") \
+#         .load("dataSource/")
+
+#     spark.sql("CREATE DATABASE IF NOT EXISTS AIRLINE_DB")
+#     spark.catalog.setCurrentDatabase("AIRLINE_DB")
+
+#     flightTimeParquetDF.write \
+#         .mode("overwrite") \
+#         .saveAsTable("flight_data_tbl")
+
+#     logger.info(spark.catalog.listTables("AIRLINE_DB"))
